b7e285a6e50df1cc9785169df006f582c009619a
Сергей Шевяков <sshevjakov@yandex.ru>
2023-12-27 10:52:09 +0300

    update urls

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_detail(request, pk):
+    template = 'blog/detail.html'
+    return render(request, template)
+
+
+def category_posts(request):
+    template = 'blog/category.html'
+    return render(request, template)
